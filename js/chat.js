60c5a7db62f46f49e07780bcd25f71ff3e3a2004
One_Ivan <ivanuradnikov439@gmail.com>
2026-05-17 23:28:47 +0300

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,5 @@
 // Токен HuggingFace для доступа к AI
-var HF_TOKEN = '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';
+var HF_TOKEN = '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';
 
 // Добавить сообщение в чат
 function addMessage(text, type) {
